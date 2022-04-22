e65844d1609ce40f6b298af9ca512567718b0158
Edu BS Home <2wiawedubs@gmail.com>
2022-04-22 15:25:25 +0200

    Start dashboard template

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,6 +1,12 @@
 <template>
   <main>
-    <h1>Dashboard</h1>
+    <NavComponent />
+    <div class="container">
+      <div class="border border-1 rounded-3 m-4 p-4">
+        <img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" height="50" alt="">
+        <p>Username</p>
+      </div>
+    </div>
   </main>
 </template>
 <script setup>
