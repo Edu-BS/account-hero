924370eb6faccc948eb502c7de7e872ec77d6247
Edu_bs <edu.bs.email@gmai.com>
2022-04-22 20:06:30 +0200

    Add user info container

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,13 +2,35 @@
   <main>
     <NavComponent />
     <div class="container">
-      <div class="border border-1 rounded-3 m-4 p-4">
-        <img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" height="50" alt="">
-        <p>Username</p>
+      <div class="border border-1 rounded-3 m-4">
+        <div class="d-flex">
+          <div class="col-3 col-md-2 m-3 me-0">
+            <img class="img-fluid" src="profile_icon.png" alt="">
+          </div>
+          <div class="col-auto ms-3 m-3">
+            <p class="fw-bold fs-4">Username</p>
+          </div>
+          <div class="col-1 col-md-1 ms-auto m-3">
+            <router-link to="">
+              <img class="img-fluid" src="gear.png" alt="">
+            </router-link>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-4">
+          <div class="col-3 text-center fw-bold">
+            <p class="mb-0 text-secondary">10</p>
+            <p>Groups</p>
+          </div>
+          <!-- <div class="col-3">
+            <p>0</p>
+          </div> -->
+        </div>
       </div>
     </div>
   </main>
 </template>
-<script setup>
+<script>
+export default {
 
+}
 </script>
