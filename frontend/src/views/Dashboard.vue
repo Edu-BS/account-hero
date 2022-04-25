37c3f285a0e0d3e74c0e0d2e8ee1aea0f7cace30
Edu BS Home <2wiawedubs@gmail.com>
2022-04-25 19:45:54 +0200

    Add Create Group button

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,6 +2,7 @@
   <main>
     <NavComponent view="Dashboard" />
     <div class="container">
+      <!-- User info -->
       <div class="border border-1 rounded-3 m-4 d-lg-none">
         <div class="d-flex">
           <div class="col-3 col-md-2 m-3 me-0">
@@ -23,6 +24,12 @@
           </div>
         </div>
       </div>
+
+      <div class="text-center justify-content-center d-lg-none">
+        <!-- Increase button size -->
+          <button class="btn btn-primary btn-circle align-middle fw-bold fs-1">+</button>
+      </div>
+      
       <!-- Groups container -->
       <div class="row justify-content-center mt-4 mx-auto">
         <div v-for="group in groups" :key="group._id" class="col-lg-4 col-sm my-3">
@@ -30,6 +37,9 @@
         </div>
       </div>
     </div>
+    <footer class="footer d-sm-none fixed-bottom py-3 bg-light text-center">
+      <button class="btn btn-primary rounded-pill">Crear Grupo</button>
+    </footer>
   </main>
 </template>
 <script>
@@ -67,8 +77,44 @@ export default {
           name: "Group 3",
           description: "Group description",
         },
+        {
+          name: "Group 1",
+          description: "Group description",
+        },
+        {
+          name: "Group 2",
+          description: "Group description",
+        },
+        {
+          name: "Group 3",
+          description: "Group description",
+        },
+        {
+          name: "Group 3",
+          description: "Group description",
+        },
+        {
+          name: "Group 3",
+          description: "Group description",
+        },
+        {
+          name: "Group 3",
+          description: "Group description",
+        },
+        
       ],
     }
   }
 };
 </script>
+
+<style>
+.btn-circle {
+    width: 60px;
+    height: 60px;
+    padding: 10px 16px;
+    border-radius: 35px;
+    font-size: 24px;
+    line-height: 1.33;
+}
+</style>
