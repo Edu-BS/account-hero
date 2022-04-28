085374fb1d117f211b036cc280e4a92c839324df
Edu BS Home <2wiawedubs@gmail.com>
2022-04-28 19:29:20 +0200

    Improve groups CRUD

--- backend/middleware/groupMiddleware.js
+++ backend/middleware/groupMiddleware.js
@@ -2,26 +2,26 @@ const GroupModel = require('../models/Group');
 
 class GroupMiddleware {
    static async userInGroup (req, res, next) {
-      const { groupId } = req.params;
-      const user = req.user;
+      const { groupId } = req.body;
+      const userId = req.userId;
       const group = await GroupModel.findById(groupId)
          .then((group) => {
             return group;
          }).catch((err) => {
-            res.status(404).json({ errors: [{ message: "group error" }] });
+            return res.status(404).json({ errors: [{ message: "group error" }] });
          });
       
       if (!group) 
-         res.status(404).json({ errors: [{ message: "group not found" }] });
-      if (!group.users.includes(user.id) && group.admin != user.id) 
-         res.status(401).json({ errors: [{ message: "user not in group" }] });
+         return res.status(404).json({ errors: [{ message: "group not found" }] });
+      if (!group.users.includes(userId.id) && group.admin != userId) 
+         return res.status(403).json({ errors: [{ message: "user not in group" }] });
       
-      next();
+      return next();
    }
 
    static async userIsAdmin (req, res, next) {
-      const { groupId } = req.params;
-      const user = req.user;
+      const { groupId } = req.body;
+      const user = req.userId;
       const group = await GroupModel.findById(groupId);
       
 
@@ -29,7 +29,7 @@ class GroupMiddleware {
       if (!group) 
          res.status(404).json({ errors: [{ message: "group not found" }] });
       if (!group.admin.equals(user.id)) 
-         res.status(401).json({ errors: [{ message: "user not admin" }] });
+         res.status(403).json({ errors: [{ message: "user not admin" }] });
       
       next();
    }
