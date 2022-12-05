5e7c590fbd1f1c70fd980a5632638358bf53ec03
Edu-BS <edu.bs.email@gmail.com>
2022-12-05 14:36:50 +0100

    #13: Init HardHat project

---
+++ blockchain/hardhat/hardhat.config.ts
@@ -0,0 +1,16 @@
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "dotenv/config";
+import "solidity-coverage";
+import "hardhat-deploy";
+import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+};
+
+export default config;
